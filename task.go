b58f5d6f1aa0b91616e5f16b3a38046cdf512e4c
hanhuateo <hanhuateo@gmail.com>
2026-08-27 18:35:49 +0800

    update: task.go json tag

--- task.go
+++ task.go
@@ -8,6 +8,6 @@ type Task struct {
 	Id          string    `json:"id"`
 	Description string    `json:"description"`
 	Status      string    `json:"status"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
