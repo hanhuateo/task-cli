66f2ee6800c05d83ed43ad119d9b90eb91534689
hanhuateo <hanhuateo@gmail.com>
2026-08-28 14:49:46 +0800

    update: task struct id field to int

--- task.go
+++ task.go
@@ -1,7 +1,7 @@
 package main
 
 type Task struct {
-	Id          string `json:"id"`
+	Id          int    `json:"id"`
 	Description string `json:"description"`
 	Status      string `json:"status"`
 	CreatedAt   string `json:"created_at"`
